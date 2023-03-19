07f96e8134e971c0ff9e24e0fdcaf69d5c868110
Firdan4 <herufirdan.alghovary@gmail.com>
2023-03-19 21:30:14 +0700

    menambahkan router-dom, menginstall react-slick, dan menambahkan komponen ReviewUserDate(data yang akan di looping) didalam folder Componen/data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+// createRoot(document.getElementById("root")).render(
+//   <RouterProvider router={router} />
+// );
+
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
