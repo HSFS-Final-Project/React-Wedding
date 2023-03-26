c8a7e69126dbc333bc6405a0d50842adabf03417
Firdan4 <herufirdan.alghovary@gmail.com>
2023-03-26 11:36:41 +0700

    menambahkan fitur navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const hendleClick = () => setNav(!nav);
   return (
     <div>
-      <nav className="relative container mx-auto mt-5">
+      <nav className="relative flex lg:block justify-between items-center container mx-auto mt-5">
         <div className="flex items-center justify-between">
           <div className="">
             <Link to="/" className="text-3xl font-bold text-logoColor">
@@ -36,6 +39,47 @@ const Navbar = () => {
             <a
               href="#"
               className="text-xl text-white p-3 px-6 pt-2 bg-buttonColor rounded-lg baseline hover:bg-gray md:block"
+            >
+              Masuk
+            </a>
+          </div>
+        </div>
+        <button onClick={hendleClick} className="lg:hidden z-10 ">
+          {!nav ? (
+            <FaBars className=" bg-pinkColor h-[40px] w-[40px] p-[5px] rounded text-white hover:bg-slate-700" />
+          ) : (
+            <FaTimes className=" bg-pinkColor h-[40px] w-[40px] p-[5px] rounded text-white hover:bg-slate-700" />
+          )}
+        </button>
+        <div
+          className={
+            !nav
+              ? "hidden"
+              : "absolute flex flex-col top-20 left-10 gap-10 shadow-xl w-[85%] rounded-lg p-16 justify-center items-center bg-white z-50"
+          }
+        >
+          <a href="#" className="text-xl text-smallTextColor cursor-pointer">
+            Preset/Design
+          </a>
+          <Link
+            to="/Portofolio"
+            className="text-xl text-smallTextColor cursor pointer"
+          >
+            Portofolio
+          </Link>
+          <a href="#" className="text-xl text-smallTextColor cursor pointer">
+            Lainnya
+          </a>
+          <div className="lg:flex flex-row space-x-5">
+            <a
+              href="/src/form-page.html"
+              className="text-xl text-smallTextColor font-semibold p-3 px-6 pt-2"
+            >
+              Daftar
+            </a>
+            <a
+              href="#"
+              className="text-xl text-white p-3 px-6 pt-2 bg-buttonColor rounded-lg baseline hover:bg-gray lg:block"
             >
               Masuk
             </a>
