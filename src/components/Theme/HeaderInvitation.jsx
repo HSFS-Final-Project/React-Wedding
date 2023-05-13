6934fbe97ce5ca4a1bc06f8e3204307ef536d7de
Firdan4 <herufirdan.alghovary@gmail.com>
2023-05-13 14:55:17 +0700

    fix

---
+++ src/components/Theme/HeaderInvitation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeaderInvitation = () => {
+  return (
+    <div className="fixed h-screen float-left w-[65%]">
+      <img
+        className="absolute object-cover -z-10 w-full h-full"
+        src="https://cdn-uploads.our-wedding.link/9d024560-e7fc-11ed-b2db-9ff0c0d42cfe.jpg"
+        alt=""
+      />
+      <p className="mt-[550px] ml-20 font-dancing text-5xl text-white">
+        Design & <br />
+        Preset
+      </p>
+    </div>
+  );
+};
+
+export default HeaderInvitation;
